c6096ca9ef01daf0a4a08aa157c7ad260bef67ad
Adipapa <34327444+Adipapa@users.noreply.github.com>
2025-03-22 11:48:01 +0000

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,1 +1,10 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api'; // Update if hosted elsewhere
+
+export const registerUser = (userData) => axios.post(`${API_URL}/auth/register`, userData);
+export const loginUser = (credentials) => axios.post(`${API_URL}/auth/login`, credentials);
+export const getEvents = () => axios.get(`${API_URL}/events`);
+export const purchaseTicket = (ticketData) => axios.post(`${API_URL}/tickets/purchase`, ticketData);
+export const redeemTicket = (qrCode) => axios.post(`${API_URL}/tickets/redeem`, { qrCode });
 
