031c333f2a5b073bbf771da1644fc24761885fdd
Adipapa <34327444+Adipapa@users.noreply.github.com>
2025-03-22 11:53:59 +0000

    Update and rename App.jsx to App.js

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import TicketRedemption from './components/TicketRedemption';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/redeem" element={<TicketRedemption />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
