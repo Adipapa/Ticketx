7c87f29502449cf04082a7c75a504048b02944f8
Adipapa <34327444+Adipapa@users.noreply.github.com>
2025-03-22 11:48:42 +0000

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,1 +1,17 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="p-4 bg-blue-600 text-white">
+      <h1 className="text-xl">TicketX</h1>
+      <div>
+        <Link to="/" className="mr-4">Home</Link>
+        <Link to="/events" className="mr-4">Events</Link>
+        <Link to="/login">Login</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
 
